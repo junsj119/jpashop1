e3aa6a7767f7fbed3471d0191eccbf2fa1be3834
junsj119 <wjstjddud119@naver.com>
2022-07-05 23:29:01 +0900

    07/05

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    //아이템 등록하러 가는 곳
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    //아이템 등록 요청
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    //상품 조회
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+     }
+}
--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -2,27 +2,35 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final ItemService itemService;
 
+    //회원가입 하러 가는 길
     @GetMapping("/members/new")
     public String createForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
+    //회원 가입
     @PostMapping("/members/new")
     public String create(@Valid MemberForm form , BindingResult result){
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
@@ -37,5 +45,44 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    //회원 목록
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+    //상품 수정 불러오기
+    @GetMapping(value = "/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    //상품 수정
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form){
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
